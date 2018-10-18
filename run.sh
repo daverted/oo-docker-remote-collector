2854c4335a34e97d31564b6c402ca0282900d370
Tim Veil <tjveil@gmail.com>
2018-10-18 08:36:22 -0400

    fixxing logging

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # you can add -l to write collector logs to stdout instead of file
-/opt/takipi/bin/takipi-service -nfd
+(/opt/takipi/bin/takipi-service -nfd &) && (tail --retry -f -n +1 /opt/takipi/log/bugtale_service.log)

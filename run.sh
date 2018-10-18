adbafc8ca8091f7f64bcc1c5a588aad4f4e4bdaa
Tim Veil <tjveil@gmail.com>
2018-10-18 08:27:58 -0400

    tailing log for better info

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # you can add -l to write collector logs to stdout instead of file
-/opt/takipi/bin/takipi-service -nfd
+(/opt/takipi/bin/takipi-service -nfd -l &) && (tail --retry -f -n +1 /opt/takipi/log/bugtale_service.log)

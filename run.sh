77da500de866a1ca817c532fd8faa28fe58649af
Tim Veil <tjveil@gmail.com>
2018-11-08 14:41:40 -0500

    trying to get tail working

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "executing run.sh"
+
 # you can add -l to write collector logs to stdout instead of file
-(/opt/takipi/bin/takipi-service -nfd &) && (tail -F -n +1 /opt/takipi/log/bugtale_service.log)
+(/opt/takipi/bin/takipi-service -nfd &) && (tail -F /opt/takipi/log/bugtale_service.log)
